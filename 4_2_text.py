aaf0af18e6f78a657fb67166498a3b36bf48fb1c
JaeHyeong Ko <kojae2423@naver.com>
2022-01-24 00:39:14 +0900

    first commit

---
+++ 4_2_text.py
@@ -0,0 +1,10 @@
+print("hello world")
+print('''
+Hello
+world
+''')
+
+print("'1'+'1'",'1'+'1')
+print('Hello world'*98)
+print("len('Hello world'*98)",len('Hello world'*98))
+print("'Hello world'.replace('world','universe')",'Hello world'.replace('world','universe'))
---
+++ 9_pypi.py
@@ -0,0 +1,5 @@
+import pandas
+house = pandas.read_csv('house.csv')
+print(house)
+print(house.head(1))
+print(house.describe())
